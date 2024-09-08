7db1bd4062581a5e7070b15d8fd0e9150bff2e77
Aayush Choudhary <aayush.c2684@gmail.com>
2024-09-08 18:01:05 +0530

    Added class private variable to handle the different query parameters of search_configure Created a function to construct the google search url based on the parameters provided by the user

--- search_configure.cpp
+++ search_configure.cpp
@@ -8,7 +8,6 @@ class GoogleSearch{
 
         std::string __api_key; // Google custom search json API key
         std::string __cx_key; // Google custom search json Search Engine ID
-        std::string __search_url = "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json";
 
         const int __filter[2] = {0, 1}; // Controls turning on or off the duplicate content filter.
         const int __c2coff[2] = {0, 1}; // Enables or disables Simplified and Traditional Chinese Search.
@@ -40,41 +39,192 @@ class GoogleSearch{
         }
         
     public:
+        // Class Public Variables
+
+        int _num_results; // Results to return for the search
+        int _start_index; // Position to start from when getting search results
+        int _filter; // turn on or off duplicate content
+        int _c2coff; // turn on or off Simplified and Traditional Chinese Search
+        int _date_restrict; // Whether to restrict search based on dates. "1" for yes and "0" for no
+        
+        std::string _extra_query; // Extra queries to include in the search terms
+        std::string _exact_terms; // A phrase that all documents in the search results must contain
+        std::string _exclude_terms; // A word or phrase that should not appear in any documents in the search results
+        std::string _site_search; // A given site which should always be included or excluded from results. Based on *_site_search_filter*
+        std::string _site_search_filter; // Whether to Include or exclude the sites specified in *_site_search*
+        std::string _link_site; // Specifies that all search results should contain a link to a particular URL
+        std::string _or_terms; // Provides additional search terms to check for in a document, where each document in the search results must contain at least one of the additional search terms
+        std::string _date_restrict_type; // Restricts results to URLs based on date. In format dateType[number]. Example: m3 restricts to past 3 months
+        std::string _low_range; // query terms to append before the search query
+        std::string _high_range; // query terms to append after the search query
+        std::string _search_type; // Specifies the search type: image. If unspecified, results are limited to webpages
+        std::string _filetype; // Restricts results to files of a specified extension
+        std::string _rights; // Filters based on licensing
+        std::string _img_size; // Returns images of a specified size
+        std::string _img_type; // Returns images of a type
+        std::string _img_color_type; // Returns black and white, grayscale, transparent, or color images
+        std::string _img_dominant_color; // Returns images of a specific dominant color
+        std::string _language; // Language to search google in. Defualt is 'English'
+        std::string _interface_language; // Sets the user interface language
+        std::string _user_country; // Country to search from in google
+        std::string _result_country; // Country to search for in google
+        std::string _safe; // Whether to show explicit results.
+        std::string _sort; // Return results in specific order
+
+        std::string _search_terms; // Query to search for on google
+        std::string _search_url; // Google Custom Search url
+        
         // Class Public Functions
 
         // Class Constructor
         GoogleSearch(
-            const int _num_results = 10, // Results to return for the search
-            const int _start_index = 0, // Position to start from when getting search results
-            const int _filter = 0, // turn on or off duplicate content
-            const int _c2coff = 0, // turn on or off Simplified and Traditional Chinese Search
-            const int _date_restrict = 0, // Whether to restrict search based on dates. "1" for yes and "0" for no
+            const std::string search_terms, // Query to search for on google
+
+            const int num_results = 10, // Results to return for the search
+            const int start_index = 0, // Position to start from when getting search results
+            const int filter = 0, // turn on or off duplicate content
+            const int c2coff = 0, // turn on or off Simplified and Traditional Chinese Search
+            const int date_restrict = 0, // Whether to restrict search based on dates. "1" for yes and "0" for no
             
-            const std::string _extra_query = "", // Extra queries to include in the search terms
-            const std::string _exact_terms = "", // A phrase that all documents in the search results must contain
-            const std::string _exclude_terms = "", // A word or phrase that should not appear in any documents in the search results
-            const std::string _site_search = "", // A given site which should always be included or excluded from results. Based on *_site_search_filter*
-            const std::string _site_search_filter = "", // Whether to Include or exclude the sites specified in *_site_search*
-            const std::string _link_site = "", // Specifies that all search results should contain a link to a particular URL
-            const std::string _or_terms = "", // Provides additional search terms to check for in a document, where each document in the search results must contain at least one of the additional search terms
-            const std::string _date_restrict_type = "", // Restricts results to URLs based on date. In format dateType[number]. Example: m3 restricts to past 3 months
-            const std::string _low_range = "", // query terms to append before the search query
-            const std::string _high_range = "", // query terms to append after the search query
-            const std::string _search_type = "", // Specifies the search type: image. If unspecified, results are limited to webpages
-            const std::string _filetype = "", // Restricts results to files of a specified extension
-            const std::string _rights = "", // Filters based on licensing
-            const std::string _img_size = "", // Returns images of a specified size
-            const std::string _img_type = "", // Returns images of a type
-            const std::string _img_color_type = "", // Returns black and white, grayscale, transparent, or color images
-            const std::string _img_dominant_color = "", // Returns images of a specific dominant color
+            const std::string extra_query = "", // Extra queries to include in the search terms
+            const std::string exact_terms = "", // A phrase that all documents in the search results must contain
+            const std::string exclude_terms = "", // A word or phrase that should not appear in any documents in the search results
+            const std::string site_search = "", // A given site which should always be included or excluded from results. Based on *_site_search_filter*
+            const std::string site_search_filter = "", // Whether to Include or exclude the sites specified in *_site_search*
+            const std::string link_site = "", // Specifies that all search results should contain a link to a particular URL
+            const std::string or_terms = "", // Provides additional search terms to check for in a document, where each document in the search results must contain at least one of the additional search terms
+            const std::string date_restrict_type = "", // Restricts results to URLs based on date. In format dateType[number]. Example: m3 restricts to past 3 months
+            const std::string low_range = "", // query terms to append before the search query
+            const std::string high_range = "", // query terms to append after the search query
+            const std::string search_type = "", // Specifies the search type: image. If unspecified, results are limited to webpages
+            const std::string filetype = "", // Restricts results to files of a specified extension
+            const std::string rights = "", // Filters based on licensing
+            const std::string img_size = "", // Returns images of a specified size
+            const std::string img_type = "", // Returns images of a type
+            const std::string img_color_type = "", // Returns black and white, grayscale, transparent, or color images
+            const std::string img_dominant_color = "", // Returns images of a specific dominant color
 
-            const std::string _language = "lang_en", // Language to search google in. Defualt is 'English'
-            const std::string _interface_language = "en", // Sets the user interface language
-            const std::string _user_country = "us", // Country to search from in google
-            const std::string _result_country = "countryUS", // Country to search for in google
-            const std::string _safe = "active", // Whether to show explicit results.
-            const std::string _sort = "descending" // Return results in specific order
+            const std::string language = "lang_en", // Language to search google in. Defualt is 'English'
+            const std::string interface_language = "en", // Sets the user interface language
+            const std::string user_country = "us", // Country to search from in google
+            const std::string result_country = "countryUS", // Country to search for in google
+            const std::string safe = "active", // Whether to show explicit results.
+            const std::string sort = "ascending" // Return results in specific order
+        ){
+            this->_search_terms = search_terms;
 
-        ){}
-        ~GoogleSearch(){}
+            this->_num_results = num_results;
+            this->_start_index = start_index;
+            this->_filter = filter;
+            this->_c2coff = c2coff;
+            this->_date_restrict = date_restrict;
+
+            this->_extra_query = extra_query;
+            this->_exact_terms = exact_terms;
+            this->_exclude_terms = exclude_terms;
+            this->_site_search = site_search;
+            this->_site_search_filter = site_search_filter;
+            this->_link_site = link_site;
+            this->_or_terms = or_terms;
+            this->_date_restrict_type = date_restrict_type;
+            this->_low_range = low_range;
+            this->_high_range = high_range;
+            this->_search_type = search_type;
+            this->_filetype = filetype;
+            this->_rights = rights;
+            this->_img_size = img_size;
+            this->_img_type = img_type;
+            this->_img_color_type = img_color_type;
+            this->_img_dominant_color = img_dominant_color;
+
+            this->_language = language;
+            this->_interface_language = interface_language;
+            this->_user_country = user_country;
+            this->_result_country = result_country;
+            this->_safe = safe;
+            this->_sort = sort;
+
+            this->construct_search_url();
+        }
+
+        // Class Destructor
+        ~GoogleSearch(){
+            this->__api_key = "";
+            this->__cx_key = "";
+
+            this->_num_results = 10;
+            this->_start_index = 0;
+            this->_filter = 0;
+            this->_c2coff = 0;
+            this->_date_restrict = 0;
+
+            this->_extra_query = "";
+            this->_exact_terms = "";
+            this->_exclude_terms = "";
+            this->_site_search = "";
+            this->_site_search_filter = "";
+            this->_link_site = "";
+            this->_or_terms = "";
+            this->_date_restrict_type = "";
+            this->_low_range = "";
+            this->_high_range = "";
+            this->_search_type = "";
+            this->_filetype = "";
+            this->_rights = "";
+            this->_img_size = "";
+            this->_img_type = "";
+            this->_img_color_type = "";
+            this->_img_dominant_color = "";
+
+            this->_language = "lang_en";
+            this->_interface_language = "en";
+            this->_user_country = "us";
+            this->_result_country = "countryUS";
+            this->_safe = "active";
+            this->_sort = "ascending";
+        }
+
+        // Create search url used to search on google
+        // Omiting and Adding appropriate parameters only
+        void construct_search_url(){
+            std::string cx_key = this->__cx_key;
+            std::string api_key = this->__api_key;
+
+            std::string search_url = "https://www.googleapis.com/customsearch/v1?q=" + this->_search_terms + "&key=" + api_key;
+
+            search_url += this->_num_results > 0 ? "&num=" + std::to_string(this->_num_results) : "";
+
+            search_url += "&start=" + std::to_string(this->_start_index);
+            search_url += "&lr=" + this->_language;
+            search_url += "&safe=" + this->_safe;
+            search_url += "&cx=" + this->__cx_key;
+            search_url += "&sort=" + this->_sort;
+            search_url += "&filter=" + std::to_string(this->_filter);
+            search_url += "&gl=" + this->_interface_language;
+            search_url += "&cr=" + this->_result_country;
+            search_url += "&c2coff=" + std::to_string(this->_c2coff);
+
+            search_url += this->_extra_query != "" ? "&hq=" + this->_extra_query : "";
+
+            search_url += "&hl=" + this->_interface_language;
+            
+            search_url += this->_site_search != "" ? "&siteSearch=" + this->_site_search : "";
+            search_url += this->_site_search_filter != "" ? "&siteSearchFilter=" + this->_site_search_filter : "";
+            search_url += this->_exact_terms != "" ? "&exactTerms=" + this->_exact_terms : "";
+            search_url += this->_exclude_terms != "" ? "&excludeTerms=" + this->_exclude_terms : "";
+            search_url += this->_link_site != "" ? "&linkSite=" + this->_link_site : "";
+            search_url += this->_or_terms != "" ? "&orTerms=" + this->_or_terms : "";
+            search_url += this->_date_restrict_type != "" ? "&dateRestrict=" + this->_date_restrict_type : "";
+            search_url += this->_low_range != "" ? "&lowRange=" + this->_low_range : "";
+            search_url += this->_high_range != "" ? "&highRange=" + this->_high_range : "";
+            search_url += this->_search_type != "" ? "&searchType=" + this->_search_type : "";
+            search_url += this->_filetype != "" ? "&fileType=" + this->_filetype : "";
+            search_url += this->_rights != "" ? "&rights=" + this->_rights : "";
+            search_url += this->_img_size != "" ? "&imgSize=" + this->_img_size : "";
+            search_url += this->_img_type != "" ? "&imgType=" + this->_img_type : "";
+            search_url += this->_img_color_type != "" ? "&imgColorType=" + this->_img_color_type : "";
+            search_url += this->_img_dominant_color != "" ? "&imgDominantColor=" + this->_img_dominant_color : "";
+
+            this->_search_url = search_url;
+        }
 };
