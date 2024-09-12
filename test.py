a01a65dc9e1b0af7cc1dd979da3eb29ba0ae2187
Aayush Choudhary <aayush.c2684@gmail.com>
2024-09-12 12:07:57 +0530

    Created an example for usage of angel_configure module

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-from modules.angel_configure import GoogleSearch
+from modules.angel_configure import GoogleSearch, File
 
+file = File()
 google = GoogleSearch()
 
-json_file = "json_files/search_result.json"
-google._search_google("Something", json_file)
+json_directory = "json_files/"
+json_file = "search_result.json"
+
+file.create_file(json_file, json_directory)
+
+google._search_google("Something", json_directory + json_file)
